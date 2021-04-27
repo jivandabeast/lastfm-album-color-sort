04381ee16f437ad9071f33921bb1e51b00b3e56c
jivandabeast <jivan@ramjisingh.com>
2021-04-27 13:14:17 -0400

    update config.py with information

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
-lastfmapi='last fm credential here'
+# Edit this file to fill in the settings for the script
+
+# Last.fm API key
+lastfmAPI=''
+
+# Last,fm username that you want to scrape
+lastfmUser=''
+
+# Time period for the album scrape (overall|7day|1month|3month|6month|12month)
+timePeriod=''
+
+# Number of results you're looking for
+count=''
