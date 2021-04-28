38cbdb4bfbc0b69993c957568f42c7b00d70fb51
jivandabeast <jivan@ramjisingh.com>
2021-04-28 02:24:07 -0400

    Bug fixes, starting to get ready for color sorting

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 import json
+import re
+import shutil
+import os
+import time
+from process import getDominantColor
 import config
-
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
 
 def lastfm_get(method, data):
     headers = {}
@@ -40,20 +40,50 @@ def itunes_get(term):
     request = requests.get(url, headers=headers, params=payload)
     return request
 
-lastAlbums = lastfm_get('user.gettopalbums', config.lastfmUser).json()
+def fix_url(url):
+    partOne = re.search(r'(^.*\/)(100x100bb)(.jpg$)', url).group(1)
+    partTwo = re.search(r'(^.*\/)(100x100bb)(.jpg$)', url).group(3)
+    url = partOne + "100000x100000-999" + partTwo
+    return url
+
+def reject_types(result):
+    if result['kind'] == 'music-video':
+        return False
+    else:
+        return True
+
+def force_url(iInfo):
+    for result in iInfo['results']:
+        if reject_types(result):
+            return result['artworkUrl100']
+        else:
+            pass
+
 coverURL = []
+colors = ["red", "orange", "yellow", "spring green", "green", "turquoise", "cyan", "ocean", "blue", "violet", "magenta", "raspberry", "black", "white"]
+
+lastAlbums = lastfm_get('user.gettopalbums', config.lastfmUser).json()
+
+# Verify and correct file structure
+if not os.path.exists('input'):
+    os.makedirs('input')
+
+if not os.path.exists('output'):
+    os.makedirs('output')
+
+for color in colors:
+    if not os.path.exists('output/' + color):
+        os.makedirs(os.makedirs('output/' + color))
 
 count = 0
 for album in lastAlbums['topalbums']['album']:
-    # Temporary counter to limit the amount of API requests during testing
-    count += 1
-    
     # print rank, album title, and artist & generate itunes search terms
     print(album['@attr']['rank'], album['name'], album['artist']['name'], album['mbid'])
     search = album['name'] + " " + album['artist']['name']
 
     # Temporary check to only do the first few entries in the request
-    if count <= 5:
+    #if str(album['@attr']['rank']) == str(18):
+    if count <= 20:
         # Get last.fm album information
         lastInfo = lastfm_get('album.getInfo', album).json()
         # Get iTunes song information
@@ -61,13 +91,70 @@ for album in lastAlbums['topalbums']['album']:
         
         # Iterate over iTunes search results to find matching track number & titles (for additional verification)
         # When a match is found, store the artwork URL in the coverURL list
+        pointer = 1
         for result in itunesInfo['results']:
-            if str(result['trackNumber']) == str(lastInfo['album']['tracks']['track'][0]['@attr']['rank']):
-                if str(result['trackName']) == str(lastInfo['album']['tracks']['track'][0]['name']):
-                    print("They're the same")
-                    coverURL.append(result['artworkUrl100'])
+            # print('Trying result %d/%d' % (pointer, itunesInfo['resultCount']))
+            try:
+                if str(result['trackNumber']) == str(lastInfo['album']['tracks']['track'][0]['@attr']['rank']):
+                    if str(result['trackName']) == str(lastInfo['album']['tracks']['track'][0]['name']):
+                        print("They're the same")
+                        coverURL.append(result['artworkUrl100'])
+                        break
+                if itunesInfo['resultCount'] == pointer:
+                    print('Error, no more entries -- forcing value')
+                    coverURL.append(force_url(itunesInfo))
                     break
+            except IndexError:
+                # For some reason, some albums don't return a tracklist on last.fm
+                # As a result, we will resort to matching artist name and album title instead (not perfect)
+                if str(result['artistName']).lower() == str(lastInfo['album']['artist']).lower():
+                    if str(result['collectionName']).lower() == str(lastInfo['album']['name']).lower():
+                        print("They're the same")
+                        coverURL.append(result['artworkUrl100'])
+                        break
+                    if str(result['trackName']).lower() == str(lastInfo['album']['name']).lower():
+                        print("Close match found, working with it")
+                        coverURL.append(result['artworkUrl100'])
+                        break
+                if itunesInfo['resultCount'] == pointer:
+                    print('Index Error, no more entries -- forcing value')
+                    coverURL.append(force_url(itunesInfo))
+                    break
+            except KeyError:
+                # Some entries will show up as singles, not tracks in an album
+                # Skip these usually, unless there are no more results to check or it was the only one
+                if itunesInfo['resultCount'] == pointer:
+                    print('Key Error, no more entries -- forcing value')
+                    coverUrl.append(force_url(itunesInfo))
+                    break
+                else:
+                    pass
+            except Exception as e:
+                # If all else fails, spit out the error and keep it moving
+                print('---')
+                print('Error:', e)
+                print('---')
+                print(result)
+                print('---')
+                print(lastInfo)
+            pointer += 1
+        print('---')
+        time.sleep(4)
     else:
         break
 
-print(coverURL)
+    # Temporary counter to limit the amount of API requests during testing
+    count += 1
+
+    # Help with the rate limiting, 20/minute
+    time.sleep(4)
+
+count = 0
+for url in coverURL:
+    url = fix_url(url)
+    response = requests.get(url, stream=True)
+    fileName = "input/" + str(count) + ".jpg"
+    with open(fileName, 'wb') as outFile:
+        shutil.copyfileobj(response.raw, outFile)
+    del response
+    count += 1
